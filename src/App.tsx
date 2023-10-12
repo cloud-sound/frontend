db5ef4ae62ff22962376bcaee4e721f6794e9a49
dong-onion <>
2023-10-12 14:22:12 +0900

    개발 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import Footer from 'components/layouts/Footer';
+import Navbar from 'components/layouts/Navbar';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>hi</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
