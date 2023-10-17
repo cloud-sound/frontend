488f39b8774450fd79673cd16df570a68b93b181
dong-onion <>
2023-10-17 16:30:28 +0900

    feat: header 컴포넌트 구현

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,4 @@
 export const cloudsoundLogo = require('./cloudsoundLogo.png');
+export const companyIntroductionHeaderImage = require('./companyIntroductionHeaderImage.png');
+export const installationCaseHeaderImage = require('./installationCaseHeaderImage.png');
+export const questionAnswerHeaderImage = require('./questionAnswerHeaderImage.png');
---
+++ src/utility/pageInfo.ts
@@ -0,0 +1,19 @@
+const INFO = {
+  installationCase: {
+    title: '설치 사례',
+    subTitle: '디자인과 종류가 다양한',
+    tabs: ['방음 부스', '방음 공사'],
+  },
+  questionAnswer: {
+    title: '질문 / 답변',
+    subTitle: '궁금한 건 무엇이든지 문의하세요',
+    tabs: ['질문 / 답변'],
+  },
+  quoteRequest: {
+    title: '견적 의뢰',
+    subTitle: '궁금한 건 무엇이든지 문의하세요',
+    tabs: ['견적 의뢰'],
+  },
+};
+
+export default INFO;
